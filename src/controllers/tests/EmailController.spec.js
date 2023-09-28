596169edda859719af8223bc08616b9925e30508
josedhonatas <dhonatas@academico.ufs.br>
2023-09-27 23:37:23 -0300

    teste_finnale

--- src/controllers/tests/EmailController.spec.js
+++ src/controllers/tests/EmailController.spec.js
@@ -1,37 +1,64 @@
 // Describe = declara bloco de teste - testes suites
 // it or test = declara um unico teste unitario - tests cases
 // expect => assercoes do resultado - validar resultados
-
-const EmailController = require('../EmailController');
-const EmailQueue = require('../../queue/MailQueue');
+const MailQueue = require('../../queue/MailQueue');
+const { sendEmail } = require('../emailController');
 
 jest.mock('../../queue/MailQueue');
 
-describe("Email Controller", ()=> {
-  test("Shout sent email sucessfuly", async () => { 
+describe('sendEmail', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should send an email successfully and return 200', async () => {
     const request = {
       body: {
-        email: "test@example.com",
-        firstName: "John",
-        lastName: "Wick"
-      }
-    } 
+        email: 'test@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
     const reply = {
       code: jest.fn().mockReturnThis(),
-      send: jest.fn(), 
-    }
+      send: jest.fn(),
+    };
 
-    const template = `Olá ${request.body.firstName} ${request.body.lastName}, sua assinatura foi confirmada! Para acessar seus recursos exclusivos você precisa basta clicar aqui.`
+    await sendEmail(request, reply);
 
-    await EmailController.sendEmail(request, reply);
-
-    expect(EmailQueue.add).toHaveBeenCalledTimes(1);
-    expect(EmailQueue.add).toHaveBeenCalledWith({
-      to: "test@example.com",
+    expect(MailQueue.add).toHaveBeenCalledTimes(1);
+    expect(MailQueue.add).toHaveBeenCalledWith({
+      to: 'test@example.com',
       from: process.env.EMAIL_FROM,
-      subject: "Assinatura Confirmada",
-      text: template
+      subject: 'Assinatura Confirmada',
+      text: `
+        Olá John Doe, sua assinatura foi confirmada!
+        Para acessar seus recursos exclusivos você precisa basta clicar aqui.
+    `,
     });
-    expect(reply.code).toHaveBeenCalledWith(200)
-  })
-})
+    expect(reply.code).toHaveBeenCalledWith(200);
+    expect(reply.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle errors and return 500 status code', async () => {
+    const request = {
+      body: {
+        email: 'test@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+    const reply = {
+      code: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+
+    MailQueue.add.mockRejectedValue(new Error('Some error'));
+
+    await sendEmail(request, reply);
+
+    expect(MailQueue.add).toHaveBeenCalledTimes(1);
+    expect(reply.code).toHaveBeenCalledWith(500);
+    expect(reply.send).toHaveBeenCalledWith('Internal Server Error');
+  });
+});
